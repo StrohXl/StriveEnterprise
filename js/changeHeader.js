d14bbbbfaf4471c6143c831ef887b565c8e035d5
StrohXl <xaviermayora20@gmail.com>
2024-07-03 14:04:52 -0400

    new changes

--- js/changeHeader.js
+++ js/changeHeader.js
@@ -10,13 +10,40 @@ const changeHeader = () => {
       navSecondary.classList.add("nav-secondary-active");
     }
   } else {
-    if (navPrimary.classList.contains("nav-primary-active")) {
-      navPrimary.classList.remove("nav-primary-active");
+    if (navPrimary.classList.contains("open-menu")) {
+      if (!navPrimary.classList.contains("nav-primary-active")) {
+        navPrimary.classList.remove("nav-primary-active");
+      }
+      
     }
+    if (!navPrimary.classList.contains("open-menu")) {
+      if (navPrimary.classList.contains("nav-primary-active")) {
+        navPrimary.classList.remove("nav-primary-active");
+      }
+      
+    }
+
     if (navSecondary.classList.contains("nav-secondary-active")) {
       navSecondary.classList.remove("nav-secondary-active");
     }
   }
 };
-changeHeader()
+const openMenu = () => {
+  const positionScroll = document.documentElement.scrollTop;
+  if (!navPrimary.classList.contains("open-menu")) {
+    navPrimary.classList.add("open-menu");
+  } else if (navPrimary.classList.contains("open-menu")) {
+    navPrimary.classList.remove("open-menu");
+  }
+
+  if (!navPrimary.classList.contains("nav-primary-active")) {
+    navPrimary.classList.add("nav-primary-active");
+  }
+  else if (positionScroll < sectionContact.clientHeight - 100) {
+    if (navPrimary.classList.contains("nav-primary-active")) {
+      navPrimary.classList.remove("nav-primary-active");
+    }
+  }
+};
+changeHeader();
 window.addEventListener("scroll", changeHeader);
--- js/playVideoReview.js
+++ js/playVideoReview.js
@@ -1,9 +1,13 @@
 const video = document.getElementById('video-review')
 const video2 = document.getElementById('video-review2')
 const video3 = document.getElementById('video-review3')
+const video4 = document.getElementById('video-review4')
+const video5 = document.getElementById('video-review5')
 const contentVideoButton = document.getElementsByClassName('content-video-button')[0]
 const contentVideoButton2 = document.getElementsByClassName('content-video-button2')[0]
 const contentVideoButton3 = document.getElementsByClassName('content-video-button3')[0]
+const contentVideoButton4 = document.getElementsByClassName('content-video-button4')[0]
+const contentVideoButton5 = document.getElementsByClassName('content-video-button5')[0]
 
 const playVideo=()=>{
     contentVideoButton.classList.add('remove')
@@ -16,4 +20,12 @@ const playVideo2=()=>{
 const playVideo3=()=>{
     contentVideoButton3.classList.add('remove')
     video3.play()
+}
+const playVideo4=()=>{
+    contentVideoButton4.classList.add('remove')
+    video4.play()
+}
+const playVideo5=()=>{
+    contentVideoButton5.classList.add('remove')
+    video5.play()
 }
